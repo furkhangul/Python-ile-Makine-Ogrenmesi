0ebf2c2c07590fe7a76d11c7fea42afb103d0559
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-13 22:52:35 +0300

    3

---
+++ 0.3 Verinin Python'dan Yüklenmesi ve içeri alınması.py
@@ -0,0 +1,27 @@
+"""
+Python üzerinden yapılan indirmeler paket dosyası şekilnde açılır.
+
+pip kurulumu yapılması gerekiyor.
+
+python.org sitesine giriş yapılıp
+
+Downloads kısmından en güncel sürümü indirip kurulum sağlanılır.
+
+Kurulum yapılırken çıkan menüde tüm paket dosyalarının seçilmesi gerekiyor.
+
+İndirilen paket dosyalarının içinde "pip" modülünün olmasını kontrol edilir.
+
+python indirildikten sonra CMD ekranına girilip python --version komutu ile kurulum kontrol edilir.
+
+"""
+
+"""
+IDE kurulumu için Pycharm'a sitesi üzerinden Communication sürümü indirilir.
+
+Free kullanım için ücretsiz sürüm seçilir.
+
+Kurulum sağlanıldıktan sonra add kısmı üzerinden pythonu import delse programa import edebiliyoruz.
+
+Ekstra kütüphane indirme işlemleri için ise:
+Menü Çubuğu > Settings > Python Interpretter > Add > "Kütüphane ismi"
+"""
