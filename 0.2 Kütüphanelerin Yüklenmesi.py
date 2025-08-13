9abaec532516a2fdb13f390356aa9547315e32b4
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-13 22:51:54 +0300

    2

---
+++ 0.2 Kütüphanelerin Yüklenmesi.py
@@ -0,0 +1,33 @@
+"""
+Gelecek yıllarda yapay zeka ve makine öğrenmesi:
+
+--->Karar verme otomatikleştirilmesi
+-İleride daha zamanlı veri akışını değerlendirip daha hızlı akiyon alabilecek bir yapıya dönüşmektedir.
+-İnsan kararlarını azaltıp, kararların doğruluğunu ve hata oranınını azaltacaktır.
+-Hız gerektiren durumlarda tepki süresini milisanyeye kadar indirebilecek.
+
+--->Tahmin Gücünün Artması:
+-Talep ve üretim dengesini önceden belirleyerek kaynak israfının önüne geçecek.
+-Hava durumu, pazar eğilimleri, müşteri davranışları gibi değişkenleri çok daha isabetli öngörecektir.
+-erken uyarı sistemleri ile olası krizlerin önüne geçecek.
+
+--->Kişiselleştirmedeki Rolü:
+-Eğitimde kişiye uygun ders programları oluşturacak.
+-Sağlıkta bireyin genetik ve yaşam tarzına göre tedavi planlayacak.
+-E-ticarette kullanıcıya tam ihtiyaç olan ürüneri zaman ve maliyetten tasarruf ile sağlayacak
+
+.
+.
+.
+Not:
+En önemlli alanı ise küreselde: Data Analistliği
+
+
+2005 yılına kadar üretilen toplam veri: 130 Exabyte
+2010: 120 Exabyte
+2020: 40900 Exabyte
+Terebte -> Petabyte -> Exabyte
+
+
+***Problem arttıkça, kapasite ihtiyacı artar.
+"""
