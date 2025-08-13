b41b8dbe7a99c8da7e371810c430c1f9730dd5a6
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-13 23:27:09 +0300

    6

---
+++ 0.6 Kütüphanelerin yüklenmesi .py
@@ -0,0 +1,22 @@
+"""
+Makine öğrenmesinde, makinenin veriyi anlayabilmesi ve çözümleyebilmesi
+için sürekli verileri yutması yani bir sürü veriyi birleştirip konu hakkınnda
+bilgi sahibi olması gerekmektedir.
+
+Bunun için makine öğrenmesinin en temel basamağı olan veri yükleme adımı
+devreye giriyor.
+
+Veriyi yüklemek için kütüphaneler kullanılır.
+En popülerleri ise Numpy ve pandas gibi kütüphanelere ihtiyaç duyarız.
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"""
+Basit kod mantığında önce kütüphaneler bulunur ardından kütüphaneler ile 
+gelen komutlar kullanılır.
+Bu kodları da kendi arasında ikiye ayırabiliriz.
+Veri yükleme adımı ve veri ön işleme adımı olarak. 
+"""
