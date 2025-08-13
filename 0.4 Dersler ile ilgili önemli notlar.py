97dae922d58030b40fb0b37388321c7d9075220a
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-13 22:53:14 +0300

    4

---
+++ 0.4 Dersler ile ilgili önemli notlar.py
@@ -0,0 +1,21 @@
+"""
+
+Metadoloji: Belirli bir hedefe en verimli şekilde ulaşmak.
+makine öğrenmesinde metodoloji adımları:
+
+-Problem tanımlanır.
+-Veri Toplanır.
+-Veri ön işlenmesi gerçekleştirilir.
+-Özellikler seçilir.
+-Model seçilir.
+-Eğitilir.
+-Değerlendirlir.
+-İyileştirilir veya optimize edilir diyebiliriz.
+-Uygulanır ve izlenilir.
+
+
+CRISP-DM, standart metadolojidir.
+Sektörden bağımsızdır.
+Her sektörde uygulanabilir.
+
+"""
