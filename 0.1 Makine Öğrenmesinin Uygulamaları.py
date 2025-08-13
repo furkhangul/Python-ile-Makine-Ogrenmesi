14fd8df78245e4ad52a2502f86b91a6f27e4b290
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-13 22:51:19 +0300

    1

---
+++ 0.1 Makine Öğrenmesinin Uygulamaları.py
@@ -0,0 +1,32 @@
+"""
+Makine öğrenmesi:
+-Görüntü İşleme
+-Doğal Dil İşleme
+-Tahmin ve Analiz Uygulamaları
+-Tavsiye Sistemleri
+-Otonom Sistemler
+-Sağlık
+gibi bir çok alanda faaliyet göstermektedir. Kısacası her alanda kullanılabilir.
+
+
+Makine nasıl görüntüyü işler:
+-Çok sayıda aynı veri toplanır.
+-Görüntüler boyutlandırılır, temizlendirilir, şekillendirilir ve ana özllikleri hakkında bir
+sürü bilgi toplanır.
+-Görüntüdeki kenarlar, şekiller, renk gibi özellikler belirlenir.
+-CNN yani doğal sinir ağları aracılığı ile özellikler öğrenilir.
+-Yeni görüntü geldiğinde model bunu tanır ve sınıflandırır.
+-Doğruluk oranı ölçülür, hata varsa model iyileştirilir.
+
+Makine öğrenmesinin arama motorlarında kullanımı:
+-Kullanıcının yazdığı kelimelerin anlamını ve niyetini tahmin eder.
+-En alaklı sayfaları üstte göstermek için öğrenilmiş modeller kullanılır.
+-Kullanıcının geçmiş aramalarına göre sonuçlar özelleştirilir.
+-Yazım hataları düzeltilir, arama önerisi sunulur.
+
+Endüstiride kullanımı:
+-Sensörler ile veriler analiz edilerek makinelerin ne zaman arzıa verecebileceği tesipt edilebilir.
+-Görüntü işleme ile üretim hatasız mı diye otomatik bir şekilde denetlenebilir.
+-Üretim hattında kendi kendine karar verip çalışan robotlar geliştirir.
+-Talep tahmini yapabilmektedir.
+"""
