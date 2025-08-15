6bf062855ca53d7f5e498db5570cbd88a55e2ceb
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-15 21:45:11 +0300

    15

---
+++ 1.5 Tahmin Problemleri.py
@@ -0,0 +1,15 @@
+"""
+Tahmin Problemleri
+
+Makine öğrenmesinde kullanılan tahmin uygulamalarının çoğu istatistikten gelmektedir.
+
+Veri sınıflandırmada gördüğümüz kategorik ve sayısal sınıflandırma tahminde de mevcuttur.
+Sınıflandırma, kategorik verileri ilgilendirir.(Kümesel veriler)
+Tahmin ise sayısal verileri ilgilendirir. (Sayısal veriler)
+
+Öngörü ile tahmin farklı şeylerdir.
+
+Tahmin günümüz ve durumları ele alıp ilerisi için verilen cevap iken.
+Öngörü herhangi bir şeye bağlı olmayabilir.
+|->Mesela bi veri grubunda eksik yaşı öngörü ile bulmaya çalışabiliriz.
+"""
