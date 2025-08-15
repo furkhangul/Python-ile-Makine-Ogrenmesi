ffddea4f3b3943480a7c7d3cde8d9afc446f26d1
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-15 21:53:30 +0300

    16

---
+++ 1.6 Veri Kütüphanesini İndirme.py
@@ -0,0 +1,10 @@
+"""
+Tahmin Problemleri
+csv formatındaki verileri indirmek için:
+https://bilkav.com/makine-ogrenmesi-egitimi
+adresine girip csv dosyasını indir.
+"""
+import pandas as pd
+import numpy as np
+
+veriler = pd.read_csv('satislar.csv')
