20fc5fbcb02e8db9ad2a9d5b7df9a217742da54d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-25 13:16:45 +0300

    45

---
+++ 4.5 Ödev 2.py
@@ -0,0 +1,11 @@
+"""
+Ödev 2
+-Veri Kümesini indiriniz
+-Gerekli / Gereksiz bağımsız değişkenleri bulunuz.
+-5 Farklı yönteme göre regresyon modelini çıkarınız.
+    |->MLR, PR, SVR, DT, RR
+-Yöntemlerin başarılarını karşılaştırınız.
+-10 yıl tecrübe ile 100 puan almış bir CEO ve aynı özelliklere
+Sahip bi müdürün maaşlarını 5 yöntemle de tahmin edip sonuçları
+yorumlayınız.
+"""
