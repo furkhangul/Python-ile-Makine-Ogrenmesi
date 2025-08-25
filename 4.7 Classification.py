23b141d3952308ae1bc6ce6802674a22f44ee772
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-25 15:19:47 +0300

    47

---
+++ 4.7 Classification.py
@@ -0,0 +1,36 @@
+"""
+Sınıflandırma (Classification):
+Sınıflandırma, makine öğrenmesinde denetimli öğrenme altında
+yer alan bir problemdir.
+Amaç, verilen verilerin önceden berlirlenmiş kategoriden hangisine
+ait olduğunu tahmin etmektir.
+Girdi: Özellikler
+Çıktı: Etiket
+
+Örneğin:
+E postanın olup olmadığını tahmin etmek
+Bir müşterinin kredi kartı başvurusunun onaylayıp onaylanmadığını tahmin etmek için
+Hastanın bir hastalığa sahip olup olmadığını belirlemek.
+
+Nasıl Çalışır.->
+Sınıflandırma, geçmişte etiketlenmiş verileri kullanarak bir model eğitir. Bu modele
+sınıflayıcı, denir. Eğitim tamamlandıktan sonra, model yeni ve bilinmeyen
+veriler için sınıf tahmininde bulunabailir.
+
+1-Veri Toplama
+2-Ön İşleme
+3-Öznitelik Seçimi - Dönüşümü
+4-Model Eğitimi
+5-Model test ve Değerlendirme
+6-Tahmin
+
+
+Kullanılan Classlar:
+-Lojistik Regresyon
+-Karar Ağaçları
+-Rassal Orman
+-Destek Vektörü
+-K en yakın konmşu
+-Naive Bayes
+-Yapay sinir ağları
+"""
