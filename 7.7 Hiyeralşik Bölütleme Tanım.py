7109f20d4fc3f6901104abb63480b8580e55345e
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-02 13:32:41 +0300

    77

---
+++ 7.7 Hiyeralşik Bölütleme Tanım.py
@@ -0,0 +1,20 @@
+"""
+İki farklı yaklaşım ile çalışır
+1-Agglomerative:
+Aşağıdan yukarıya doğru çalışır
+2-Divisive:
+Yukarıdan aşağıya doğru çalışır.
+
+Amaç bütün olasılıkları deneyerek çalışır.
+N tane datamız olursa n tane kümemiz olur.
+daha sonra en yakın iki komşuyu alır ve tek küme haline gelir.
+
+bu işlem tek kümeye çevrilinceye kadar devam eder.
+
+Agglomerative'da tane tane birleştirilirken
+Divisiv'da ise tane tane ayrıştırılır.
+
+burada en büyük problem hangi uzaklık ölçüm methodu kullanılacağı önemldiir.
+
+referansta önemlidir.
+"""
