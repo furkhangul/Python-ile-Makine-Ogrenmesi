5aa690c5f1e0f2eedf9bfa023c2ccbb1142ff15b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-02 13:31:26 +0300

    75

---
+++ 7.5 Küme - Bölüt sayısına karar vermek.py
@@ -0,0 +1,16 @@
+"""
+Bir diğer sorun ise k değerinin belirlenmesi. K-Mean'da biz belirlediğimiz için
+bunu yanlış belirlediğimiz durumda hatalar ile karşılaşırız.
+
+k-means algoritmasında bilgi ile ayrılmadığını bildiğimiz için nasıl yapacğımızı da bilemeyiz.
+
+K değerinin hangi kümse sayısında en iyi performans gösterdiğini bulmak veri bilimcinin bildiği bir iş.
+
+Bunun belirlenmesi için özel bir formül geliştirimiştir.
+WCSS denilen bu algoritma
+Within - cluser sums of squares
+
+Bu formül ile çıkarılan grafikler ile en olağan küme sayısı oluşturulur.
+
+Optimum noktayı seçerek overfitting önlenir.
+"""
