4525b5c98eca201f3351d8923ae3d27125f0bf40
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-04 14:31:54 +0300

    83

---
+++ 8.3 Rainforce Learning.py
@@ -0,0 +1,28 @@
+"""
+Takviyeli / Pekiştirmeli Öğrenme
+Reinforced Learning
+:
+Pekiştirmeli öğrenme, yazılımı en iyi sonuçları elde etmek üzere kararlar vermesi
+için eğiten bir makine öğrenimi tekniğidir.
+
+İnsanların hedeflerine ulaşmak için kullandıkları deneme yanılma öğrenme sürecini
+taklit eder. Hedefiniz doğrultusunda çalışan yazılım eylemleri pekiştirilirken,
+hedeften uzaklaştıran eylemler göz ardı edilir.
+
+RL algoritmaları, verileri işleyen bir ödül ve ceza paradigması kullanır.
+Her eylemin geri bildirimlerinden öğrenirler ve nihai sonuçlara ulaşmak için
+en iyi işleme yollarını kendi kendilerine keşfederler.
+
+Algoritmalar ayrıca ertelenmiş tatmin yeteneğine de sahiptir.
+
+En iyi genel strateji kısa vadeli fedakarlıklar gerektirebilir.
+
+Kullanımı
+
+Robotlar ve Oyunlar (Aplha Go)
+
+
+----Geri besleme yöntemi ile geliştiriliyor diyebiliriz.
+
+
+"""
