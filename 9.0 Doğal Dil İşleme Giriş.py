91c4021f5d22f5b3012f9c76a2e797a45e65986e
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-06 16:11:11 +0300

    90

---
+++ 9.0 Doğal Dil İşleme Giriş.py
@@ -0,0 +1,71 @@
+"""
+Doğal Dil İşleme
+
+Doğal dil, makine dilinden ayrılan bi dil. Makine dili = 1 ve 0'lardan oluşan
+ve makinenin anladığı düşük seviyedeki dildir.
+
+Doğal dil ise insanların konuştuğu dile verilen addır.
+
+DDİ Bölümleri:
+---Hedefler:
+*NLU
+*NLG
+---Yaklaşımlar
+*Lingustik dilbilim yaklaşımı
+*İstatiksel Yaklaşımlar
+*Hibrit Yaklaşımlar - hem dilbilimi hem istatistiksel karışımı
+
+->Lingustik yaklaşımı biraz daha yavaş ama daha başarılıdır.
+->İstatistiksel daha hızlı ama daha hatalı
+
+
+Lingustik Açılacak olursa alt dalları.
+|> Morpholohy
+|> Syntax
+|> Semantic
+|> Pragmatic
+
+İstatistiksel Yaklaşımın alt dalları:
+|> N-gram
+|> TF-IDF
+|> Word-gram
+|> BOW
+
+
+Bu modeller yazar tanıma için kullanılır. Verilen kitabın hangi yazara ait olduğunu
+bulmaya çalışır.
+POS-Tagging yöntemi kullanılır.
+
+
+Metnin sınıfalandırılmasına kullanılabilir. Metnin hangi bölümde, hangi alanda
+yazıldığını bulmaya çalışır.
+
+Duygusal Kutupsallık, Fikir Madenciliğinde kullanılır. Metnin hangi duygu ve fikir üzerinde
+kurulduğunu anlamamıza yardımcı olur.
+
+Anamoli Yakalamda kullanılır. Yazılan bir mesajda bu mesajı inceleyip bu mesajın
+istatistiksel özellikleri ile bize bilgi verebilir. Saldırı olduğunu tespit edebilir.
+
+Doğa Dil İŞleme kütüphaneleri
+NTLK
+SpaCy
+Standford NLP
+OpenNLP
+Rapid Automatic Keyword Extraction
+Amuelle Word Cloud
+Tensor Flow**
+
+Tensor Flow python kütüphanelerinden bir tanesidir.
+Dil öğrenme kütüphanelerinin en popüler kütüphanelerindendir.
+
+
+Türkçe DDİ Kütüphanesi
+Zemberek
+İTÜ
+TSPELL
+YILDIZ TEKNİK ÜNİVERSİTESİ : KEMİK
+WORDNET
+TrMorph
+TSCorpus
+METU- Sabancı Tree Bank ve ITU Doğrulama Kümesi
+"""
