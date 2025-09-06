3b91ac1f1310aea1f1111b56bf9372f977a73f7f
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-09-06 16:33:54 +0300

    91

---
+++ 9.1 Doğal Dil İşleme Adımları.py
@@ -0,0 +1,49 @@
+"""
+NLP Adımları
+Bütün makine öğrenmeleri hemen hemen aynı şeyleri gerçekleştiriyor.
+
+Veriler elimize geliyor
+Verileri makineye Veriyoruz
+Ve makine bize sonuçları veriyor.
+
+Veriyorduk borsa verilerini,yarın ne çıkacak diye tahminlerini soruyorduk.
+
+Aynı şekilde Doğal Dil İşleme de aynı şekilde çalışır.
+
+1. Veri Toplama (Data Collection)
+NLP'nin ilk adımı, üzerinde çalışılacak verilerin toplanmasıdır.
+Bu veri metin, konuşma veya diğer dil biçimlerinde olabilir.
+Veri, bir kitap, makale, tweet, sohbet geçmişi veya blog yazısı gibi kaynaklardan alınabilir.
+
+
+2. Ön İşleme
+Veri üzerinde analiz yapılabilmesi için ilk adım genellikle temizleme ve düzenleme işlemleridir.
+ADIMLAR:
+Küçük harfe dönüştürme:
+Metni normalize etmek için tüm harfler küçük harfe çevrilebilir.
+
+Özel karakterlerin kaldırılması:
+Noktalama işaretleri, sayılar gibi gereksiz karakterler temizlenir.
+
+Stop words:
+"ve", "bir", "de" gibi anlam taşımayan kelimeler kaldırılır.
+
+Kelime Köklerine İndirgeme:
+Kelimelerin kök formuna dönüştürülmesi sağlanır.
+Örneğin, "koşmak" ve "koşuyor" kelimeleri tek bir köke indirgenebilir.
+
+
+Tokenizasyon:
+Metin, anlamlı birimler olan kelimelere (ya da token'lara) bölünür.
+
+
+3. Öznitelik Çıkartma:
+Metinden sayısal özellikler (öznitelikler) çıkarılır.
+Bu, makine öğrenmesi modellerinin kullanabilmesi için önemlidir.
+
+4. Model Seçimi ve Eğitim
+Doğal dil işleme görevine göre uygun bir model seçilir ve eğitim süreci başlatılır.
+.
+.
+.
+"""
